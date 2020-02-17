f46c44826091d2faa155be02f8c13ef8ec2e1306
Walter Marinho <waltersmarinho@edu.unifil.br>
2020-02-17 18:43:26 -0300

    Started server communication and timestamp inclusion

--- config/session.php
+++ config/session.php
@@ -3,7 +3,6 @@ include('db.php');
 session_start();
 
 $user_id = mysqli_real_escape_string($conn, $_SESSION['id']);
-var_dump($user_id) . "<br>";
 /*if (!isset($_SESSION['id'])) {
     header("Location: login.php");
     die();
@@ -14,11 +13,10 @@ var_dump($user_id) . "<br>";
 $query = "SELECT * FROM funcionarios WHERE id = '$user_id' ";
 $result = mysqli_query($conn, $query);
 $user_info = mysqli_fetch_array($result, MYSQLI_ASSOC);
-var_dump($user_info);
+
 
 
 //Store info in session
 $_SESSION['name'] = $user_info['nome'];
 $_SESSION['password'] = $user_info['password'];
 $_SESSION['id'] = $user_info['id'];
-var_dump($_SESSION);
--- main.php
+++ main.php
@@ -2,11 +2,40 @@
 require('config/config.php');
 require('config/db.php');
 require('config/session.php');
+
+// Attributing variables to include in table
+date_default_timezone_set("America/Sao_Paulo");
+$id = $_SESSION['id'];
+$date = getdate(time());
+print_r($date);
+$horario = date("h:i:sa");
+
+//query to include dates
+
+
+
+/*if (isset($_POST['btn-exit'])) {
+    $query = "SELECT horario_saida FROM horarios WHERE data = '$date'";
+    $result = mysqli_query($conn,$query);
+}*/
 ?>
 <?php include("header.php"); ?>
 <p>Welcome <?php echo $_SESSION['name']; ?>! </p>
 <h1>Main</h1>
-<button type="submit" class="btn btn-start">Registrar entrada</button>
-<button type="submit" class="btn btn-exit">Registrar saída</button>
+<form method="POST" action="">
+    <input type="submit" name="btn-start" class="btn btn-start">Registrar entrada</button>
+</form>
+<?php
+if (isset($_POST['btn-start'])) {
+    $query = "INSERT INTO horarios (id, data) VALUES ($id, $date)";
+    $result = mysqli_query($conn, $query);
+    $query1 = "INSERT INTO horarios (horario_ent) VALUES ($horario)";
+    $result1 = mysqli_query($conn, $query1);
+    echo "Sucesso ao incluir horario de entrada";
+}
+?>
+<form method="POST" action="">
+    <input type="submit" name="btn-exit" class="btn btn-exit">Registrar saída</button>
+</form>
 
 <?php include("footer.php"); ?>
