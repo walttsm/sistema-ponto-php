03269d706a7ccb769378f4939797471f5419a8d0
Walter Marinho <waltersmarinho@edu.unifil.br>
2020-02-17 16:37:54 -0300

    Login functionality added

--- login.php
+++ login.php
@@ -2,22 +2,28 @@
 require('config/config.php');
 require('config/db.php');
 session_start();
-//Get info from server
-$query = 'SELECT * FROM funcionarios';
 
-//Associate the table to a variable
-$result = mysqli_query($conn, $query);
-$employees = mysqli_fetch_all($result, MYSQLI_ASSOC);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //Associating form data to variables
+    $user_id = mysqli_real_escape_string($conn, $_POST['id']);
+    $user_pass = mysqli_real_escape_string($conn, $_POST['password']);
 
-//Check if button is pressed
+    //Get if user is in table:
+    $query = "SELECT id FROM funcionarios WHERE id = '$user_id' and password = '$user_pass'";
+    $result = mysqli_query($conn, $query);
 
-if (isset($_POST['submit'])) {
-    foreach ($employees as $employee) {
-        if ($employee['id'] === $_POST['id'] && $employee['password'] === $_POST['password']) {
-            header('localhost/sistema-ponto/main.php');
-        }
+    $count = mysqli_num_rows($result);
+
+    // Checks if user is valid
+    if ($count == 1) {
+        $_SESSION['id'] = $user_id;
+        header("Location: main.php");
+    } else {
+        echo "Incorrect user or password, please try again or contact the system admin";
     }
 }
+
+
 
 
 
@@ -35,10 +41,10 @@ if (isset($_POST['submit'])) {
     <h1>Login</h1>
     <form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>">
         <label for="id">Id: </label>
-        <input name="id"> <br>
+        <input type="text" name="id"> <br>
         <label for="password">Password: </label>
-        <input type="password" name="password" value=""> <br>
-        <input type="submit" name="login" value="login" class="btn">
+        <input type="password" name="password"> <br>
+        <input type="submit" name="login" value="submit" class="btn">
     </form>
 </body>
 
